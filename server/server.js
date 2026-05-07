f3bb03086eddced791516a0c81040c1dfa6f310e
sajan26v <sajanthanv26@gmail.com>
2026-05-07 10:02:14 +0530

    The Server include in git hub

---
+++ server/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+
+// load env varibales
+dotenv.config();
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+// Test route
+app.get('/', (req, res) => {
+    res.send("Railway Reservation API Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>  {
+    console.log(`Server running on port ${PORT}`);
+})
